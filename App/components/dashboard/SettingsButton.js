8ab25d77c6d8f5d7fa09628505760be1753faec3
Jake Ritter <jakeritter77@gmail.com>
2023-01-14 20:31:20 -0800

    - Implemented FAQ.js

--- App/components/dashboard/SettingsButton.js
+++ App/components/dashboard/SettingsButton.js
@@ -7,21 +7,21 @@ import {
 } from "react-native";
 import React from "react";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import { useNavigation } from "@react-navigation/native";
 import CustomModal from "../standard/CustomModal";
 import Settings from "../../views/Settings";
+import FAQ from "../../views/FAQ";
 
 const SettingsButton = () => {
   const [modalVisible, setModalVisible] = React.useState(false);
 
   return (
     <CustomModal
-      toggleBtnTitle={<Ionicons name="settings" size={24} color="black" />}
+      toggleBtnTitle={<Ionicons name="help-circle" size={24} color="black" />}
       toggleBtnStyle={styles.toggleBtnStyle}
       modalVisible={modalVisible}
       setModalVisible={setModalVisible}
     >
-      <Settings />
+      <FAQ />
     </CustomModal>
   );
 };
--- App/views/FAQ.js
+++ App/views/FAQ.js
@@ -1,15 +1,121 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import React, { forwardRef } from "react";
 import CustomScrollView from "../components/standard/CustomScrollView";
 
+const faqs = [
+  {
+    question: "How do I create a new bill?",
+    answer:
+      "1. Navigate to the My Bills tab.\n\n" +
+      '2. Tap the "New +" button in the top left of the screen.\n\n' +
+      "3. Fill out the required information.\n\n" +
+      '4. Tap "Save Changes" when you are finished.',
+  },
+  {
+    question: "How do I edit an existing bill?",
+    answer:
+      "1. Navigate to the My Bills tab.\n\n" +
+      "2. Tap the desired bill to expand the bills information.\n\n" +
+      '3. Tap the "Edit" button. \n\n' +
+      "4. Make changes to desired fields" +
+      '5. Tap "Save Changes" when you are finished.',
+  },
+  {
+    question: "How do I delete an existing bill?",
+    answer:
+      "1. Navigate to the My Bills tab.\n\n" +
+      "2. Tap the desired bill to expand the bills information.\n\n" +
+      '3. Tap the "Delete" button. \n\n' +
+      '4. Tap "Yes" when asked for confirmation',
+  },
+  {
+    question: "How do I confirm a bill's payment has been made?",
+    answer:
+      "1. Navigate to either the Dashboard or My Payments tab.\n\n" +
+      '2. Find the desired bill in the "Next Due Bills" section and tap the green checkmark icon next to the bill.\n\n' +
+      "3. Enter/confirm payment details. \n\n" +
+      '4. Tap "Save Changes" when finished.',
+  },
+  {
+    question: "Can I delete a confirmed payment?",
+    answer:
+      "1. Navigate to the My Payments tab.\n\n" +
+      '2. Find the desired payment in the "Previous Payments" section and tap the red "X" icon next to the payment.\n\n' +
+      '3. Tap "Yes" when asked for confirmation. \n\n',
+  },
+  {
+    question: "How does BillSpotter store my bill information?",
+    answer:
+      "BillSpotter saves all bill information to the user's device only.\n\n" +
+      "BillSpotter does not store any information remotely and information cannot be transfered between devices.",
+  },
+];
+
 const FAQ = () => {
+  const populateFaqs = () => {
+    let questions = [];
+
+    for (let i = 0; i < faqs.length; i++) {
+      questions.push(<Question key={i} faq={faqs[i]} />);
+    }
+
+    return questions;
+  };
+
+  return <CustomScrollView>{populateFaqs()}</CustomScrollView>;
+};
+
+const Question = ({ faq }) => {
+  const [collapsed, setCollapsed] = React.useState(true);
+
   return (
-    <CustomScrollView>
-      <Text>FAQ</Text>
-    </CustomScrollView>
+    <View style={styles.question}>
+      <Pressable
+        style={styles.questionHeader}
+        onPress={() => setCollapsed(!collapsed)}
+      >
+        <Text style={styles.questionHeaderText}>{faq.question}</Text>
+        <Text style={styles.headerText}>{collapsed ? "+" : "-"}</Text>
+      </Pressable>
+      <Text
+        style={{ ...styles.questionText, display: collapsed ? "none" : "flex" }}
+      >
+        {faq.answer}
+      </Text>
+    </View>
   );
 };
 
 export default FAQ;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  question: {
+    backgroundColor: "white",
+    borderRadius: 5,
+    margin: "5%",
+    borderWidth: 10,
+  },
+  questionHeader: {
+    backgroundColor: "black",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  questionHeaderText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    backgroundColor: "black",
+  },
+  questionText: {
+    fontSize: 16,
+    padding: "5%",
+  },
+});
